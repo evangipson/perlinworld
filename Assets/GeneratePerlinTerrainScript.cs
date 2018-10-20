7d72813f817a585d1a4526c74bf3d600df72194e
Evan Gipson <evangipson@gmail.com>
2018-10-20 18:13:00 -0500

    Remove Update function, fine tune persistance

--- Assets/GeneratePerlinTerrainScript.cs
+++ Assets/GeneratePerlinTerrainScript.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 
 public class GeneratePerlinTerrainScript : MonoBehaviour {
 
+    private System.Random random = new System.Random();
     // you can modify these variables via the inspector in unity
-    public float amplitude = 60.0f;
-    public float frequency = 2.0f;
-    public int width = 200;
-    public int height = 200;
+    public int width = 500;
+    public int height = 500;
+    public float amplitude = 128.0f;
+    public float frequency = 4.0f;
     public float textureScale = 1.0f;
+    // how many layers of "detail" will we have in the world
+    public int octaves = 3;
     // private variables used by this file not referenced or open in unity
     private float[,] heights;
     // Splatmap data is stored internally as a 3d array of floats, so declare a new empty array ready for your custom splatmap data:
     private float[,,] splatmapData;
     private Terrain terrain;
-    private System.Random random = new System.Random();
     private Renderer rend;
     // to keep track if we have changes
     private float _oldAmplitude;
@@ -33,15 +35,19 @@ public class GeneratePerlinTerrainScript : MonoBehaviour {
     }
     void FixedUpdate()
     {
-        if (UnityVariableChanged())
-        {
-            CaptureUnityFacingVariables();
-            terrain.terrainData = GeneratePerlinTerrain(terrain.terrainData);
-            RecolorMap();
-        }
+        //if (UnityVariableChanged())
+        //{
+        //    CaptureUnityFacingVariables();
+        //    terrain.terrainData = GeneratePerlinTerrain(terrain.terrainData);
+        //    RecolorMap();
+        //}
     }
     void CaptureUnityFacingVariables()
     {
+        // set our frequency to be based off the map size
+        frequency = ((height / 100) + (width / 100)) / 2 > 4.0f ? ((height / 100) + (width / 100)) / 2 : 4.0f;
+
+        // now save them
         _oldAmplitude = amplitude;
         _oldWidth = width;
         _oldHeight = height;
@@ -69,8 +75,6 @@ public class GeneratePerlinTerrainScript : MonoBehaviour {
     {
         // a grid of floats, so each point on our map has a float associated to it.
         heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
-        // how many layers of "detail" will we have in the world
-        int octaves = random.Next(2, 5);
         for (int x = 0; x < terrain.terrainData.heightmapWidth; x++)
         {
             for (int y = 0; y < terrain.terrainData.heightmapHeight; y++)
@@ -168,9 +172,11 @@ public class GeneratePerlinTerrainScript : MonoBehaviour {
 
     float CalculateHeight(int x, int y, int octaves)
     {
-        float persistence = (float)(random.Next(25, 75) * .01); // pick a random persistence, 0-100%
+        // how much is each layer going to effect the final map?
+        float persistence = 0.5f;
         float noise = 0f;
-        float maxValue = 0; // used for clamping final resultz
+        // used for clamping final resultz
+        float maxValue = 0;
         float localFrequency = frequency;
         float localAmplitude = amplitude;
         float xCoord;
